7698ee5215f485cf18dd018354523311700c0492
Ishani Nuwanthika(QA Journal by Ish) <nuwanthikaishani@gmail.com>
2025-11-08 11:57:19 +0530

    updated basic js codes

--- basics1.js
+++ basics1.js
@@ -1,2 +1,19 @@
 
 console.log("Hello Ishani");
+
+let a = 4;
+console.log(a);
+console.log(typeof(a));
+
+let b = 987.98;
+console.log(typeof(b));
+
+let c = "Ishani Nuwanthika";
+console.log(typeof(c));
+
+let required = true;
+console.log(typeof(required));
+
+let d = a + b;
+console.log(d);
+
---
+++ basics2.js
@@ -0,0 +1,28 @@
+
+const flag = true;
+
+if(!flag){
+
+    console.log("condition satisfied")
+
+}
+else{
+
+    console.log("condition not satisfied")
+
+}
+
+
+let i =0;
+
+while(i<10){
+    i++;
+    console.log(i);
+
+}
+
+console.log("=======================================================");
+
+for(let k=0;k<=5;k++){
+    console.log(k);
+}
